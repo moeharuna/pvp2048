ed183942063a3a849678b35294e6a73c2b30b1ab
moeharuna <flandare@protonmail.com>
2022-05-22 13:28:08 +0500

    player can read state now

--- cpp/src/gamelogic.h
+++ cpp/src/gamelogic.h
@@ -1,11 +1,13 @@
+#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <memory>
 
 #pragma once
+const size_t NUMBER_OF_PLAYERS =2;
 const size_t BOARD_WIDTH = 4;
 const size_t BOARD_SIZE = 16;
-const size_t NUMBER_OF_PLAYERS =2;
+
 enum GameAction {
   MOVE_LEFT,
   MOVE_RIGHT,
@@ -14,24 +16,34 @@ enum GameAction {
 };
 
 
-using score_t = uint64_t;
-class Game;
+
+struct GameState;
 class Player {
 public:
   virtual ~Player() =default;
   virtual GameAction get_action() = 0;
-  virtual score_t& player_score() = 0;
-  virtual void update_game_state(const Game&) = 0; //FIXME: there is no way to actually read state from Game class.
+  virtual void read_game_state(const GameState&) = 0;
 };
-using Winner = std::shared_ptr<Player>;
-using player_ptr = std::unique_ptr<Player>;
 
+
+
+using score_t = uint64_t;
+struct GameState {
+  public:
+  std::string to_string() const;
+
+  std::array<std::array<uint32_t, BOARD_WIDTH>, BOARD_WIDTH> gameField; // power of two
+  bool your_turn_now;
+  score_t your_score;
+  score_t enemy_score;
+};
+
+using player_ptr = std::unique_ptr<Player>;
 class Game {
 public:
-  std::string printable_game_board() const; // Mostly for debugging
   Game(player_ptr p1, player_ptr p2);
   ~Game();
-  Winner game_loop();
+  void game_loop();
 private:
   struct impl;
   std::unique_ptr<impl> pImpl;
