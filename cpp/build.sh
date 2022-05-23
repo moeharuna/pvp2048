5c89c60944f81124157b03e61aaaf43d7404f9cb
moeharuna <flandare@protonmail.com>
2022-05-23 16:57:40 +0500

    grpc building

--- cpp/build.sh
+++ cpp/build.sh
@@ -3,6 +3,8 @@
 export CC=clang
 export CXX=clang++
 conan install -if build/ . || exit
+protoc -I ../grpc/ ../grpc/game.proto --plugin=protoc-gen-grpc="$(which grpc_cpp_plugin)" --grpc_out=./src/proto_out || exit
+protoc -I ../grpc/ ../grpc/game.proto --cpp_out=./src/proto_out || exit
 cmake -B build/ || exit
 cd build || exit
 make -j
